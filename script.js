e9e2c2452700318898b2a2837d98e2a3b533817a
Shinrael <mircoelena89@gmail.com>
2024-02-19 15:36:17 +0100

    [edit] number

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const lastName = prompt('Il tuo cognome?');
 
 const color = prompt ('Il tuo colore preferito?');
 
-console.log(name, lastName, color);
+const number = 24;
+
+console.log(name, lastName, color, number);
 
 document.getElementById ('password') .innerHTML = `
-<p> Ciao ${name + ' ' + lastName}, ecco la tua password sicura <strong> ${name + lastName + color + 24} </strong> . </p>
+<p> Ciao ${name + ' ' + lastName}, ecco la tua password sicura <strong> ${name + lastName + color + number} </strong> . </p>
 `
