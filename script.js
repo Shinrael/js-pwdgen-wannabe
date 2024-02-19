f24c2e100dd41d3eee8688dd6e7eeeb3d1afc13f
Shinrael <mircoelena89@gmail.com>
2024-02-19 15:29:48 +0100

    [edit] innerHTML

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ const color = prompt ('Il tuo colore preferito?');
 
 console.log(name, lastName, color);
 
-document.getElementById ('password') .innerHTML = name + lastName + color + 24;
+document.getElementById ('password') .innerHTML = `
+<p> Ciao ${name + ' ' + lastName}, ecco la tua password sicura <strong> ${name + lastName + color + 24} </strong> . </p>
+`
